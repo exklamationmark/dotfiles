9dc64996f3862c89c36b48712cfbb652f494d6f9
exklamationmark <tonghuukhiem@gmail.com>
2024-12-16 05:19:59 -0800

    Orchestrate the setup process using script and nix (home-manager)
    Various things couldn't be setup with nix/home-manager yet, so they
    should still use the original setup script.

---
+++ lib/install.bash
@@ -0,0 +1,138 @@
+#!/bin/bash
+
+# -------------------- colors --------------------
+RED='\e[0;31m'
+GREEN='\e[0;32m'
+YELLOW='\e[0;33m'
+BLUE='\e[0;34m'
+NONE='\e[0m'
+
+red(){
+  local msg=$1
+  echo -e $RED"$msg"$NONE
+}
+
+green(){
+  local msg=$1
+  echo -e $GREEN"$msg"$NONE
+}
+
+yellow(){
+  local msg=$1
+  echo -e $YELLOW"$msg"$NONE
+}
+
+blue(){
+  local msg=$1
+  echo -e $BLUE"$msg"$NONE
+}
+
+# -------------------- install --------------------
+
+is_installed(){
+  local package=$1
+  if dpkg --get-selections | grep -q "^$package[[:space:]]*install$" >/dev/null
+  then
+    return 0
+  fi
+  return 1
+}
+
+install_apt() {
+  local package=$1
+  sudo apt-get install $package -y
+}
+
+install_deb() {
+  # install a .deb file from a URL
+  local package=$1
+  local url=$2
+
+  if [[ -n "$url" ]]
+  then
+    curl $url > /tmp/$package.deb
+  fi
+  sudo dpkg -i /tmp/$package.deb
+}
+
+install() {
+  local installer=$1
+  local package=$2
+  if is_installed $package
+  then
+    green "$package is already installed"
+    return 0
+  fi
+
+  # run apt-get install for apt packages
+  if ( ! is_installed $package ) && [[ $installer == "apt" ]]
+  then
+    yellow "Installing $package using $installer ..."
+    install_apt $package
+  fi
+
+  # run dpkg -i for .deb files
+  if ( ! is_installed $package ) && [[ $installer == "deb" ]]
+  then
+    local package_url=$3
+    yellow "Installing $package using $installer ..."
+    install_deb $package $package_url
+  fi
+
+  # install from source
+  if ( ! is_installed $package ) && [[ $installer == "source" ]]
+  then
+    local install_function=${package}_from_source
+    yellow "Installing $package using $installer ..."
+    $install_function ${@:3}
+  fi
+}
+
+pause(){
+  read -p "$*"
+}
+
+configure_apt_mirror() {
+  local backup=$(mktemp)
+  local tmp=$(mktemp)
+  local src="/etc/apt/sources.list.d/ubuntu.sources"
+
+  local pattern="http://[a-z]\+\.archive\.ubuntu\.com/ubuntu/"
+  local ubuntu_mirror="mirror://mirrors.ubuntu.com/mirrors.txt"
+  local ubc_mirror="http://mirror.it.ubc.ca/ubuntu/"
+
+  yellow "Backing up $src at $backup ..."
+  cp $src $backup
+  yellow "Updating $src to use Ubuntu's mirrors list ..."
+  cat $backup | sed -e "s|${pattern}|${ubuntu_mirror}|g" > $tmp
+  sudo mv $tmp $src
+  rm -f $tmp
+}
+
+configure_google_chrome_ppa() {
+	wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo tee /etc/apt/keyrings/google-chrome.asc >/dev/null
+	echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/google-chrome.asc] http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/google-chrome.list >/dev/null
+}
+
+configure_mozilla_ppa() {
+	wget -q -O - https://packages.mozilla.org/apt/repo-signing-key.gpg | sudo tee /etc/apt/keyrings/mozilla.asc >/dev/null
+	echo "deb [signed-by=/etc/apt/keyrings/mozilla.asc] https://packages.mozilla.org/apt/ mozilla main" | sudo tee /etc/apt/sources.list.d/mozilla.list >/dev/null
+	echo '
+Package: *
+Pin: origin packages.mozilla.org
+Pin-Priority: 1000
+
+Package: firefox*
+Pin: release o=Ubuntu
+Pin-Priority: -1' | sudo tee /etc/apt/preferences.d/mozilla >/dev/null
+}
+
+configure_1password_ppa() {
+	wget -q -O - https://downloads.1password.com/linux/keys/1password.asc | sudo tee /etc/apt/keyrings/1password.asc >/dev/null
+	echo 'deb [arch=amd64 signed-by=/etc/apt/keyrings/1password.asc] https://downloads.1password.com/linux/debian/amd64 stable main' | sudo tee /etc/apt/sources.list.d/1password.list >/dev/null
+
+	sudo mkdir -p /etc/debsig/policies/AC2D62742012EA22/
+	wget -q -O - https://downloads.1password.com/linux/debian/debsig/1password.pol | sudo tee /etc/debsig/policies/AC2D62742012EA22/1password.pol >/dev/null
+	sudo mkdir -p /usr/share/debsig/keyrings/AC2D62742012EA22
+	wget -q -O - https://downloads.1password.com/linux/keys/1password.asc | sudo gpg --dearmor --output /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg >/dev/null
+}
---
+++ setup.bash
@@ -0,0 +1,64 @@
+#!/bin/bash
+set -exuo
+
+source lib/install.bash
+source lib/configure.bash
+
+# ==============================================================================
+# This section is run from the bootstrapped repo (from .zip archive)
+# It installs a minimal setup of tools, clone the repo using git
+# and then run the actual setup from there.
+#
+# This way, we can mutate the repo (e.g: adding work email, secrets, etc),
+# yet track the changes as and commit them for future use.
+# ------------------------------------------------------------------------------
+configure_apt_mirror
+sudo apt-get clean
+sudo apt-get update
+install apt curl
+install apt git
+
+GH_ORG_DIR=${HOME}/workspace/src/github.com/exklamationmark
+REPO=${GH_ORG_DIR}/dotfiles
+
+mkdir -p ${GH_ORG_DIR}
+cd ${GH_ORG_DIR}
+git clone https://github.com/exklamationmark/dotfiles.git
+cd ${REPO}
+# ==============================================================================
+# NOTE: From here onwards, we are in the cloned repo
+
+# Upgrades
+sudo apt-get upgrade -y
+
+# Install things that haven't switched to home-manager (or can't work with it)
+# ==============================================================================
+configure_google_chrome_ppa
+configure_mozilla_ppa
+configure_1password_ppa
+
+sudo apt-get update
+
+install apt google-chrome-stable
+
+sudo snap remove --purge firefox
+sudo apt remove firefox -y
+install apt firefox
+
+install apt 1password
+# ==============================================================================
+
+# Nix
+curl --proto '=https' --tlsv1.2 -sSf \
+	-L https://install.determinate.systems/nix | \
+	sh -s -- install
+export PATH=/nix/var/nix/profiles/default/bin:$PATH
+
+# Setup with home-manager
+ln -sf ${PWD}/home-manager ${HOME}/.config/home-manager
+cd ~/.config/home-manager
+nix run home-manager switch -- -b backup
+
+red "For the best experience, reboot using:"
+blue "sudo reboot"
+pause
