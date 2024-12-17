eaec3e3ecc08c7b9af1218b09dec31c27ae7ea51
exklamationmark <tonghuukhiem@gmail.com>
2024-12-17 03:07:50 -0800

    style: Add more settings

--- home-manager/home-manager/style/ubuntu/ubuntu.nix
+++ home-manager/home-manager/style/ubuntu/ubuntu.nix
@@ -10,20 +10,33 @@
     settings = {
       "org/gnome/desktop/interface" = {
         color-scheme = "prefer-dark";
-        accent-color = "blue";
-        gtk-theme = "Yaru-dark";
-        icon-theme = "Yaru";
+
+        text-scaling-factor = 1.5; # Larger text
 
         clock-format = "24h";
         clock-show-date = true;
-        clock-show-seconds = false;
-        clock-show-weekday = false;
+        clock-show-seconds = true;
+        clock-show-weekday = true;
 
         enable-animations = false;
 
         enable-hot-corners = true;
       };
 
+      # Lock screen
+      "org/gnome/desktop/session" = with lib.hm.gvariant; {
+        idle-delay = mkUint32 900; # Screen goes blank after 15m
+      };
+      "org/gnome/desktop/screensaver" = with lib.hm.gvariant; {
+        lock-enabled = true;
+        lock-delay = mkUint32 3600; # Lock screen 1h after screen went blank
+      };
+
+      # Dock
+      "org/gnome/shell/extensions/dash-to-dock" = {
+        dash-max-icon-size = 32;
+        dock-position = "RIGHT";
+      };
       "org/gnome/shell" = {
         favorite-apps = [
           "org.gnome.Nautilus.desktop"
@@ -35,6 +48,13 @@
         ];
       };
 
+      # Keyboard shortcuts
+      "org/gnome/settings-daemon/plugins/media-keys" = {
+        screensaver = [ "<Shift><Control>slash" ];
+      };
+      "org/gnome/shell/keybindings" = {
+        show-screenshot-ui = [ "<Shift><Control>8" ];
+      };
     };
   };
 }
