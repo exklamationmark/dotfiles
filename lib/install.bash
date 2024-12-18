5da24c67c3b180c5d209eb17f3e7bbf9756bce72
exklamationmark <tonghuukhiem@gmail.com>
2024-12-18 00:31:01 -0800

    Refactor lib/install.bash

--- lib/install.bash
+++ lib/install.bash
@@ -8,113 +8,113 @@ BLUE='\e[0;34m'
 NONE='\e[0m'
 
 red(){
-  local msg=$1
-  echo -e $RED"$msg"$NONE
+	local msg=$1
+	echo -e $RED"$msg"$NONE
 }
 
 green(){
-  local msg=$1
-  echo -e $GREEN"$msg"$NONE
+	local msg=$1
+	echo -e $GREEN"$msg"$NONE
 }
 
 yellow(){
-  local msg=$1
-  echo -e $YELLOW"$msg"$NONE
+	local msg=$1
+	echo -e $YELLOW"$msg"$NONE
 }
 
 blue(){
-  local msg=$1
-  echo -e $BLUE"$msg"$NONE
+	local msg=$1
+	echo -e $BLUE"$msg"$NONE
 }
 
 # -------------------- install --------------------
 
 is_installed(){
-  local package=$1
-  if dpkg --get-selections | grep -q "^$package[[:space:]]*install$" >/dev/null
-  then
-    return 0
-  fi
-  return 1
+	local package=$1
+	if dpkg --get-selections | grep -q "^$package[[:space:]]*install$" >/dev/null
+	then
+		return 0
+	fi
+	return 1
 }
 
 install_apt() {
-  local package=$1
-  sudo apt-get install $package -y
+	local package=$1
+	sudo apt-get install $package -y
 }
 
 install_deb() {
-  # install a .deb file from a URL
-  local package=$1
-  local url=$2
+	# install a .deb file from a URL
+	local package=$1
+	local url=$2
 
-  if [[ -n "$url" ]]
-  then
-    curl $url > /tmp/$package.deb
-  fi
-  sudo dpkg -i /tmp/$package.deb
+	if [[ -n "$url" ]]
+	then
+		curl $url > /tmp/$package.deb
+	fi
+	sudo dpkg -i /tmp/$package.deb
 }
 
 install() {
-  local installer=$1
-  local package=$2
-  if is_installed $package
-  then
-    green "$package is already installed"
-    return 0
-  fi
+	local installer=$1
+	local package=$2
+	if is_installed $package
+	then
+		green "$package is already installed"
+		return 0
+	fi
 
-  # run apt-get install for apt packages
-  if ( ! is_installed $package ) && [[ $installer == "apt" ]]
-  then
-    yellow "Installing $package using $installer ..."
-    install_apt $package
-  fi
+	# run apt-get install for apt packages
+	if ( ! is_installed $package ) && [[ $installer == "apt" ]]
+	then
+		yellow "Installing $package using $installer ..."
+		install_apt $package
+	fi
 
-  # run dpkg -i for .deb files
-  if ( ! is_installed $package ) && [[ $installer == "deb" ]]
-  then
-    local package_url=$3
-    yellow "Installing $package using $installer ..."
-    install_deb $package $package_url
-  fi
+	# run dpkg -i for .deb files
+	if ( ! is_installed $package ) && [[ $installer == "deb" ]]
+	then
+		local package_url=$3
+		yellow "Installing $package using $installer ..."
+		install_deb $package $package_url
+	fi
 
-  # install from source
-  if ( ! is_installed $package ) && [[ $installer == "source" ]]
-  then
-    local install_function=${package}_from_source
-    yellow "Installing $package using $installer ..."
-    $install_function ${@:3}
-  fi
+	# install from source
+	if ( ! is_installed $package ) && [[ $installer == "source" ]]
+	then
+		local install_function=${package}_from_source
+		yellow "Installing $package using $installer ..."
+		$install_function ${@:3}
+	fi
 
-  # install with custom function
-  if ( ! is_installed $package ) && [[ $installer == "custom" ]]
-  then
-    local install_function=${package}_custom_install
-    yellow "Installing $package using $installer ..."
-    $install_function ${@:3}
-  fi
+	# install with custom function
+	if ( ! is_installed $package ) && [[ $installer == "custom" ]]
+	then
+		local install_function=${package}_custom_install
+		yellow "Installing $package using $installer ..."
+		$install_function ${@:3}
+	fi
 }
 
 pause(){
-  read -p "$*"
+	read -p "$*"
 }
 
 configure_apt_mirror() {
-  local backup=$(mktemp)
-  local tmp=$(mktemp)
-  local src="/etc/apt/sources.list.d/ubuntu.sources"
+	local backup=$(mktemp)
+	local tmp=$(mktemp)
+	local src="/etc/apt/sources.list.d/ubuntu.sources"
 
-  local pattern="http://[a-z]\+\.archive\.ubuntu\.com/ubuntu/"
-  local ubuntu_mirror="mirror://mirrors.ubuntu.com/mirrors.txt"
-  local ubc_mirror="http://mirror.it.ubc.ca/ubuntu/"
+	local pattern="http://[a-z]\+\.archive\.ubuntu\.com/ubuntu/"
+	local ubuntu_mirror="mirror://mirrors.ubuntu.com/mirrors.txt"
+	local ubc_mirror="http://mirror.it.ubc.ca/ubuntu/"
 
-  yellow "Backing up $src at $backup ..."
-  cp $src $backup
-  yellow "Updating $src to use Ubuntu's mirrors list ..."
-  cat $backup | sed -e "s|${pattern}|${ubuntu_mirror}|g" > $tmp
-  sudo mv $tmp $src
-  rm -f $tmp
+	yellow "Backing up $src at $backup ..."
+	cp $src $backup
+	yellow "Updating $src to use Ubuntu's mirrors list ..."
+	cat $backup | sed -e "s|${pattern}|${ubuntu_mirror}|g" > $tmp
+	sudo mv $tmp $src
+	rm -f $tmp
 }
 
 configure_google_chrome_ppa() {
