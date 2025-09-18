3ac57bd6954aaebe49c4a246463ae80666960ab7
exklamationmark <tonghuukhiem@gmail.com>
2025-09-18 10:16:12 -0700

    fix(bash): Use unlimited history

--- home-manager/home-manager/apps/bash/bashrc.d/history.bash
+++ home-manager/home-manager/apps/bash/bashrc.d/history.bash
@@ -4,11 +4,10 @@
 # ==============================================================================
 # Do not put duplicate lines or lines starting with space in the history.
 export HISTCONTROL=ignoreboth
-# Undocumented feature which sets the in-memory history size to "unlimited".
+# Sets the in-memory history size to "unlimited".
 # REF: http://stackoverflow.com/questions/9457233/unlimited-bash-history
-unset HISTSIZE
-# Do not truncate history file
-unset HISTFILESIZE
+export HISTSIZE=-1
+export HISTFILESIZE=-1
 # Change the file location because certain bash sessions truncate .bash_history file upon close.
 # REF: http://superuser.com/questions/575479/bash-history-truncated-to-500-lines-on-each-login
 export HISTFILE=~/.bash_history_all
